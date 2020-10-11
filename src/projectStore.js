527409ee25d313c246533ca35f8b423aa68313c1
SeyiAriyo <ariyoseyiariyo@gmail.com>
2020-10-10 21:03:47 -0400

    fixed work photos

--- src/projectStore.js
+++ src/projectStore.js
@@ -1,6 +1,6 @@
 const projects = [
   {
-    title: 'Saneful',
+    title: 'Binaural',
     subTitle: 'React/Node',
     techs: [
       'React.js',
@@ -14,20 +14,21 @@ const projects = [
       'Nodemon',
       'Supertest',
       'Mocha',
+      'Canvas',
+      'SineWave.js',
       'Enzyme',
     ],
-    description: 'A game that serves as a social commentary on the 2020 COVID19 pandemic. It\'s a survival game where the player is quarantined and working from home. The game is played in the browser and includes full user registration, the ability to save and load game progress and a leaderboard.',
+    description: 'Binaural generates binaural beats and audio visualizations to help users achieve altered states of consciousness.',
     imagePaths: [
-      'images/portfolio/saneful/login.png',
-      'images/portfolio/saneful/game.png',
-      'images/portfolio/saneful/leaderboard.png',
-      'images/portfolio/saneful/dashboard.png',
+      'images/portfolio/binaural/binaural-screenshot-home.png',
+      'images/portfolio/binaural/binaural-screenshot-beat-player.png',
+      'images/portfolio/binaural/binaural-screenshot-turn-on.png',
     ],
-    gitHubLink: 'https://github.com/thinkful-ei-panda/Saneful2020-client',
-    liveSite: 'https://saneful.vercel.app/'
+    gitHubLink: 'https://github.com/thinkful-ei-panda/binaural-client',
+    liveSite: 'https://binaural-client.vercel.app/'
   },
   {
-    title: 'A Training Notebook',
+    title: 'Recipes',
     subTitle: 'React/Node',
     techs: [
       'React.js',
@@ -43,38 +44,18 @@ const projects = [
       'Mocha',
       'Enzyme',
     ],
-    description: 'This project is called "a training notebook" because that is exactly what it is meant to supersede. Gym goers have carried notebooks or tracked their workouts on their phones for decades. In an effort to organize this ancient practice I have created a very simple and effective way to record and recall your workouts.',
+    description: 'The recipes app is a personal cooking assistant. You can add your favorite and current ingredients in your liquor cabinet to your digital cabinet, and the app will show you what recipes you can make with your ingredients and how! You will also be able to search for recipes by ingredient type and make a shortlist of your favorite ones to try later.',
     imagePaths: [
-      'images/portfolio/a-training-notebook/main.png',
-      'images/portfolio/a-training-notebook/workout.png',
-      'images/portfolio/a-training-notebook/landing.png',
-      'images/portfolio/a-training-notebook/login.png',
+      'images/portfolio/recipes/login.png',
+      'images/portfolio/recipes/recipes.png',
+      'images/portfolio/recipes/Recipesview.png',
+      'images/portfolio/recipes/addRecipe.png',
     ],
     gitHubLink: 'https://github.com/nickthorpe71/a-training-notebook-II-client',
     liveSite: 'https://atrainingnotebook.vercel.app/'
   },
   {
-    title: 'Alchamancer',
-    subTitle: 'Unity',
-    techs: [
-      'C#',
-      'Unity',
-      'PUN2',
-      'Xcode',
-    ],
-    description: 'Created to allow players to quickly play a strategic spell casting game against each other online from their phone. I used Unity and C# to build the game and used PUN2 for the networking. The game appears simple but through creating it I learned the importance of having strong management systems in place to help organize and optimize the application.',
-    imagePaths: [
-      'images/portfolio/alchamancer/main.png',
-      'images/portfolio/alchamancer/red-arena.png',
-      'images/portfolio/alchamancer/win-screen.png',
-      'images/portfolio/alchamancer/alcha-arena.png',
-      'images/portfolio/alchamancer/green-bandit.png',
-    ],
-    gitHubLink: 'https://github.com/nickthorpe71/Alchamancer',
-    liveSite: 'https://apps.apple.com/us/app/alchamancer/id1483929834'
-  },
-  {
-    title: 'Codificación',
+    title: 'French Moi',
     subTitle: 'React/Node',
     techs: [
       'React.js',
@@ -90,13 +71,13 @@ const projects = [
       'Mocha',
       'Enzyme',
     ],
-    description: 'An application to assist english speaking developers quickly learn key programming terms in spanish. Using spaced repetition the app presents the user with words and asks them for the translation. Providing an incorrect answer will push the word to the front of the queue, and a correct answer pushes it further back in the queue. An algorithm on the backend determines the new placement for each word.',
+    description: 'An application to assist english speaking developers quickly learn key terms in French. Using spaced repetition the app presents the user with words and asks them for the translation. Providing an incorrect answer will push the word to the front of the queue, and a correct answer pushes it further back in the queue. An algorithm on the backend determines the new placement for each word.',
     imagePaths: [
-      'images/portfolio/codificacion/dash-ss.png',
-      'images/portfolio/codificacion/learning-ss.png',
+      'images/portfolio/frenchmoi/French Moi.png',
+      'images/portfolio/frenchmoi/PastedGraphic3.png',
     ],
-    gitHubLink: 'https://github.com/nickthorpe71/spaced-repitition-nick-gage-client',
-    liveSite: 'https://codificacion.vercel.app/'
+    gitHubLink: 'https://github.com/thinkful-ei-panda/Spaced-Repetition-Rebecca-Seyi',
+    liveSite: 'https://spaced-repitition-rebecca-seyi.vercel.app'
   },
   {
     title: 'This Portfolio',
@@ -112,32 +93,10 @@ const projects = [
     ],
     description: 'Built to display my work and to provide an easy point of contact.',
     imagePaths: [
-      'images/portfolio/this-portfolio/home-page.png',
+      'images/portfolio/this-portfolio/Pasted Graphic.png',
     ],
-    gitHubLink: 'https://github.com/nickthorpe71/portfolio',
-    liveSite: 'https://nickthorpe.dev/'
-  },
-  {
-    title: 'Starwars API Search',
-    subTitle: 'React',
-    techs: [
-      'React.js',
-      'HTML',
-      'CSS',
-      'Vercel',
-      'Chai',
-      'Nodemon',
-      'Supertest',
-      'Mocha',
-      'Enzyme',
-    ],
-    description: 'A single page React client built to interface with a third party Star Wars API.',
-    imagePaths: [
-      'images/portfolio/star-wars-search/full.png',
-      'images/portfolio/star-wars-search/close-up.png',
-    ],
-    gitHubLink: 'https://github.com/thinkful-ei-panda/star-wars-search-nick',
-    liveSite: 'https://star-wars-search-eight.vercel.app/'
+    gitHubLink: 'https://github.com/SeyiAriyo/Final-Portfolio',
+    liveSite: 'https://final-portfolio-sand.vercel.app'
   },
 ];
 
